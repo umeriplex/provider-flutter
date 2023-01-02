8e88ad8ad75b5e2200bfca7ddbb56d164f57c1a0
umeriplex <shikariss24@gmail.com>
2023-01-02 15:13:49 +0500

    log in with provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:satate_management_one/providers/auth_provider.dart';
 import 'package:satate_management_one/providers/count_provider.dart';
 import 'package:satate_management_one/providers/example_one_provider.dart';
 import 'package:satate_management_one/providers/favourite_provider.dart';
@@ -8,6 +9,7 @@ import 'package:satate_management_one/screens/example_one.dart';
 
 import 'favorites/favourite_screen.dart';
 import 'home_screen.dart';
+import 'logiin_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,13 +25,14 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => CountProvider()),
         ChangeNotifierProvider(create: (_) => ExampleOneProvider()),
         ChangeNotifierProvider(create: (_) => FavouriteProvider()),
+        ChangeNotifierProvider(create: (_) => LoginProvider()),
       ],
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const FavouriteScreen(),
+        home: const LoginScreen(),
       ),
     );
   }
